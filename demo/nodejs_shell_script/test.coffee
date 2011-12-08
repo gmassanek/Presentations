b630340a25a956bb7eb6250114e258edf9459c17
Sean Massa <smassa@groupon.com>
2011-12-08 13:55:18 -0600

    added new presentations, moved stuff around

---
+++ demo/nodejs_shell_script/test.coffee
@@ -0,0 +1,6 @@
+#!/usr/bin/env coffee
+
+square = (x) -> x*x
+
+console.log square 3
+
---
+++ demo/nodejs_shell_script/test.js
@@ -0,0 +1,8 @@
+#!/usr/bin/env node
+var argv = require('optimist').argv;
+var _ = require('underscore');
+
+console.log(argv.ick);
+
+
+
